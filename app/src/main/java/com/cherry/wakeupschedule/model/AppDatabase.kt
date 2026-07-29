1fb6f0c441b7a9f1fcf989d781082c981d80da90
laoin <2908451607@qq.com>
2026-07-29 14:16:05 +0800

    feat: AppDatabase 注册多账号实体及 DAO，版本升至 2

--- app/src/main/java/com/cherry/wakeupschedule/model/AppDatabase.kt
+++ app/src/main/java/com/cherry/wakeupschedule/model/AppDatabase.kt
@@ -4,11 +4,27 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.cherry.wakeupschedule.model.dao.AccountDao
+import com.cherry.wakeupschedule.model.dao.AccountSettingsDao
+import com.cherry.wakeupschedule.model.dao.SemesterInfoDao
 
-@Database(entities = [Course::class], version = 1, exportSchema = false)
+@Database(
+    entities = [
+        Course::class,
+        AccountEntity::class,
+        AccountProfileEntity::class,
+        SemesterInfoEntity::class,
+        AccountSettingsEntity::class
+    ],
+    version = 2,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun courseDao(): CourseDao
+    abstract fun accountDao(): AccountDao
+    abstract fun semesterInfoDao(): SemesterInfoDao
+    abstract fun accountSettingsDao(): AccountSettingsDao
 
     companion object {
         @Volatile
